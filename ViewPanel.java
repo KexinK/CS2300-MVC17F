929e2ba0b122447b1dcea0ea19c3d0750b9cd465
Kexin Kang <kkx499543102@gmail.com>
2017-12-03 16:39:07 -0500

    ViewPanel

---
+++ ViewPanel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc17F;
+
+/**
+ *
+ * @author kk892515
+ */
+public final class ViewPanel extends ControlPanel{
+
+    public ViewPanel(Model model) {
+        super(model);
+    }
+
+    
+}
